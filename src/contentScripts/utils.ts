cf51719106bfa8125a01248fd42d7d9b3c62fb79
Lars Öltjenbruns <larsoeltjenbruns@gmail.com>
2021-10-18 01:23:32 +0200

    feat: increase isTargetPage timeout

--- src/contentScripts/utils.ts
+++ src/contentScripts/utils.ts
@@ -10,7 +10,7 @@ const isTargetPage = (condition: () => unknown) =>
         resolve(conditionResponse);
 
         // stop interval after 5 seconds
-      } else if ((Date.now() - start) / 1000 >= 5) {
+      } else if ((Date.now() - start) / 1000 >= 10) {
         clearInterval(interval);
         reject();
       }
