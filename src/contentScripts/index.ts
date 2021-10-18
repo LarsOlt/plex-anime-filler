de63a011f534b377f79c99d5fa010df35d9a2279
Lars Öltjenbruns <larsoeltjenbruns@gmail.com>
2021-10-18 22:54:12 +0200

    fix: DOM changes gone after rerender on scroll

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,46 +1,85 @@
 import { AnimeFillerListResponse, isTVShowSeasonPage } from "./utils";
+import stringSimilarity from "string-similarity";
+
+let animeFillerList: AnimeFillerListResponse | null = null;
 
 isTVShowSeasonPage.then(() => {
   const cellItems = document.querySelectorAll<HTMLDivElement>(
     "div[data-qa-id='cellItem']",
   );
 
+  const cellItemsWrapper = cellItems[0].parentNode as Node;
+
   const observer = new MutationObserver((mutationsList) => {
-    mutationsList.forEach(({ type }) => {
-      console.log({ type });
+    mutationsList.forEach((mutation) => {
+      populateCells(mutation.addedNodes);
     });
   });
 
-  observer.observe(cellItems[0].parentNode as Node, {
+  observer.observe(cellItemsWrapper, {
     childList: true,
   });
-
-  const getCellItemTitle = (cell: HTMLDivElement) =>
-    cell.querySelector("a[data-qa-id='metadataTitleLink']")?.textContent;
 
   chrome.runtime.sendMessage(
     "getAnimeFillerList",
     (res: AnimeFillerListResponse) => {
-      cellItems.forEach((cell, i) => {
-        const { title, type } = res[i];
+      animeFillerList = res;
 
-        if (getCellItemTitle(cell) === title) {
-          cellItems[i].innerHTML += `
-                <div style="
-                    position: absolute;
-                    top: 1em;s
-                    right: 1em;
-                    color: white;
-                    text-transform: uppercase;
-                    padding: .5em;
-                    background-color: ${type === "Filler" ? "red" : "green"};
-                    border-radius:5px;
-                    box-shadow: 0 0 3px gray;
-                    "
-                >${type}</div>
-            `;
-        }
-      });
+      console.log(res);
+
+      populateCells(cellItems);
     },
   );
 });
+
+const getCellData = (cell: Node) => {
+  const title = (cell as HTMLDivElement).querySelector(
+    "a[data-qa-id='metadataTitleLink']",
+  )?.textContent;
+
+  return {
+    title,
+    fillerInfo: animeFillerList?.find((info) => {
+      return stringSimilarity.compareTwoStrings(info.title, title!) > 0.95;
+    }),
+  };
+};
+
+const addFillerInfoToCell = (params: {
+  cell: Node;
+  fillerInfo: AnimeFillerListResponse[0];
+}) => {
+  const { cell, fillerInfo } = params;
+
+  (cell as HTMLDivElement).innerHTML += `
+            <div style="
+                position: absolute;
+                top: 1em;
+                right: 1em;
+                color: white;
+                text-transform: uppercase;
+                padding: .5em;
+                background-color: ${
+                  fillerInfo.type === "Filler" ? "red" : "green"
+                };
+                border-radius:5px;
+                box-shadow: 0 0 3px gray;
+                "
+            >${fillerInfo.type}</div>
+        `;
+};
+
+const populateCells = (cells: NodeList) => {
+  cells.forEach((cell) => {
+    const { fillerInfo } = getCellData(cell);
+
+    console.log(getCellData(cell));
+
+    if (fillerInfo) {
+      addFillerInfoToCell({
+        cell,
+        fillerInfo,
+      });
+    }
+  });
+};
